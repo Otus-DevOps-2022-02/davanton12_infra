14ff07ab4ef8c1775e5ce0974c4bdd107585dbd1
davanton12 <davanton12@mail.ru>
2022-04-12 18:51:01 +0300

    terraform-2 #6

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,1 +1,13 @@
-test
+terraform {
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "stage-terraform-state"
+    key        = "state/terraform.tfstate"
+    region     = "us-east-1"
+    access_key = "YC"
+    secret_key = "YC"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
