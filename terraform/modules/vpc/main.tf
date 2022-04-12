6fdccb4972110699267d37cc358436af261e8893
davanton12 <davanton12@mail.ru>
2022-04-12 18:44:39 +0300

    terraform-2 #5

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,28 @@
+resource "yandex_compute_instance" "vpc" {
+  name = "reddit-vpc"
+  labels = {
+    tags = "reddit-vpc"
+}
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.app_disk_image
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
