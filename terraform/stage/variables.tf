270f4819a88f8d06956109eae2382b376d06ce86
davanton12 <davanton12@mail.ru>
2022-04-12 17:54:44 +0300

    terraform-2

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -37,3 +37,17 @@ variable "instances" {
   description = "count instances"
   default     = 1
 }
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
+variable "access_key" {
+  description = "Access key"
+}
+variable "secret_key" {
+  description = "Secret key"
+}
